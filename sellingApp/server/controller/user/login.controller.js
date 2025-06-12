4d543ec3e4a7a8141ec1b30e2dc992f81c97ba17
Suryamani Gupta <manigupta28201@gmail.com>
2025-06-12 14:11:57 +0530

    Add cookie-parser dependency and update login controller for user validation; Working on use of JWT token for auth

--- sellingApp/server/controller/user/login.controller.js
+++ sellingApp/server/controller/user/login.controller.js
@@ -1,5 +1,25 @@
-const loginController = (req, res, next) => {
-  console.log(req.body);
+import userModel from "../../models/user.model";
+
+const loginController = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //! 1. Check if a newUser, then need to signUp first
+  const newUser = await userModel.findOne({ email });
+  if (newUser) {
+    return res
+      .status(400)
+      .json({ message: "Email isn't in Database,Create your account first" });
+  }
+
+  //! 2. Compare password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
+
+  //! 3. Generate JWT Token
+
+  res.status(200).json({ message: "Login successful", userId: user._id });
 };
 
 export default loginController;
